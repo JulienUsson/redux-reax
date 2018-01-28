7583cf26d2d23699efb5cfd1416bb592409651eb
Julien Usson <usson.julien@gmail.com>
2018-01-28 17:50:34 +0100

    :white_check_mark: simple tests

--- tests/index-test.js
+++ tests/index-test.js
@@ -1,9 +1,39 @@
 import expect from 'expect'
+import { createModule } from 'src'
 
-import message from 'src/index'
-
-describe('Module template', () => {
-  it('displays a welcome message', () => {
-    expect(message).toContain('Welcome to test')
+describe('Create module', () => {
+  it('create module', () => {
+    const { reducer, creators } = createModule({
+      state: {
+        count: 1,
+      },
+      mutations: {
+        increment(state) {
+          return { count: state.count + 1 }
+        },
+        incrementBy(state, amount) {
+          return { count: state.count + amount }
+        },
+      },
+      actions: {
+        incrementAsync({ commit }, timeout = 1000) {
+          return new Promise(resolve => {
+            setTimeout(() => {
+              commit('increment')
+              resolve()
+            }, timeout)
+          })
+        },
+        async incrementByAsync({ dispatch }, amount, timeout = 1000) {
+          for (let i = 0; i < amount; i++) {
+            await dispatch('incrementAsync', timeout)
+          }
+        },
+      },
+    })
+    expect(reducer).toExist()
+    expect(creators).toExist()
+    expect(reducer).toBeA(Function)
+    expect(creators).toBeA(Object)
   })
 })
